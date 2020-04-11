befbee78f03e73ef05bd82ff6a47b4209e8ed1b6
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-04-11 17:54:41 +0300

    feat: add preloader

--- src/pages/MembersPage.js
+++ src/pages/MembersPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import Preloader from '../components/Preloader';
 const MembersPage = ({ members }) => {
   return (
-    <ul>
-      {members.map(({ name, id }) => (
-        <li key={id}>{name}</li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {members.map(({ name, id }) => (
+          <li key={id}>{name}</li>
+        ))}
+      </ul>
+      <Preloader />
+    </>
   );
 };
 
