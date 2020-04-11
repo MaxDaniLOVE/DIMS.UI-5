0ef812d8e08f00ef183e7d66146ccc4d5eeb40ce
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-04-11 22:35:41 +0300

    fix: fix members years in members table

--- src/components/MembersTable/MembersTable.js
+++ src/components/MembersTable/MembersTable.js
@@ -5,14 +5,17 @@ const MembersTable = ({ members }) => {
   const headers = ['#', 'Full name', 'Direction', 'Education', 'Start', 'Age', 'Manage'];
   const membersTableBody = members.map((member, idx) => {
     const { id, name, lastName, directionId, education, startDate, birthDate } = member;
+    const stringStartDate = new Date(startDate).toLocaleDateString();
+    const ageMs = new Date().getTime() - new Date(birthDate).getTime();
+    const ageInYears = new Date(ageMs).getFullYear() - 1970;
     return (
       <tr key={id}>
         <td>{idx + 1}</td>
         <td>{`${name} ${lastName}`}</td>
         <td>{directionId}</td>
         <td>{education}</td>
-        <td>{startDate}</td>
-        <td>{birthDate}</td>
+        <td>{stringStartDate}</td>
+        <td>{ageInYears}</td>
         <td>there will be btns</td>
       </tr>
     );
