18c6dae60d97fd17bfe5b82bda685d98ea2211ef
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-04-13 11:22:38 +0300

    feat: add preloader to members task page

--- src/pages/MembersTasksPage.js
+++ src/pages/MembersTasksPage.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import Firebase from '../services/Firebase';
 import MembersTasksTable from '../components/MembersTasksTable';
+import Preloader from '../components/Preloader';
 
 class MembersTasksPage extends Component {
   constructor() {
     super();
     this.state = {
       userTasks: [],
+      isLoaded: false,
     };
   }
 
@@ -18,14 +20,14 @@ class MembersTasksPage extends Component {
     db.getUsersTasks(match.params.mid).then((newTasksData) => {
       this.setState({
         userTasks: newTasksData,
+        isLoaded: true,
       });
     });
-    console.log('there will be fetching data');
   }
 
   render() {
-    const { userTasks } = this.state;
-    return <MembersTasksTable userTasks={userTasks} />;
+    const { userTasks, isLoaded } = this.state;
+    return <>{isLoaded ? <MembersTasksTable userTasks={userTasks} /> : <Preloader />}</>;
   }
 }
 
