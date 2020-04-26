d45e10a4ce81a032b88c09a8c4b5eb2849fce8ea
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-04-26 11:26:13 +0300

    feat: add new methods to Authentication class

--- src/services/Authentication.js
+++ src/services/Authentication.js
@@ -4,15 +4,36 @@ export default class Authentication {
   auth = firebase.auth();
 
   registerNewUser = async ({ email, password }) => {
-    this.auth.createUserWithEmailAndPassword(email, password).catch((error) => {
+    try {
+      const newUser = await this.auth.createUserWithEmailAndPassword(email, password);
+      return newUser;
+    } catch (error) {
       console.error(error.message);
+    }
+  };
+
+  onStatusChanged = () => {
+    this.auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log('user logged in');
+      } else {
+        console.log('user logged out');
+      }
     });
   };
 
   login = async ({ email, password }) => {
     try {
       const userData = await this.auth.signInWithEmailAndPassword(email, password);
-      console.log(userData.user.email);
+      return userData;
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  logout = async () => {
+    try {
+      await this.auth.signOut();
     } catch (error) {
       console.error(error.message);
     }
