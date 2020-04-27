a55a22ff069592f5fa1478fa688ade1f74937b8e
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-04-27 19:41:40 +0300

    feat: rewrite auth page using reactstrap

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Label } from 'reactstrap';
+import { AvForm, AvGroup, AvInput } from 'availity-reactstrap-validation';
 import { SubmitButton, SuccessButton } from '../../UI/Buttons';
+
 import './loginForm.scss';
 
 const LoginForm = ({ onFormChange, onSubmit, inputs, isFormValid, isRegisterMode, onSwitchMode }) => {
-  const inputsField = inputs.map(({ label, id, type }) => (
-    <div className='login-form__input' key={id}>
-      <label htmlFor={id}>
+  const inputsField = inputs.map(({ label, id, type, validationPattern }) => (
+    <AvGroup className='login-form__input' key={id}>
+      <Label htmlFor={id}>
         {label}
-        <input type={type} id={id} onChange={onFormChange} />
-      </label>
-    </div>
+        <AvInput
+          name={id}
+          type={type}
+          id={id}
+          onChange={onFormChange}
+          validate={{
+            required: { value: true, errorMessage: 'Please enter a name' },
+            pattern: {
+              value: validationPattern,
+              errorMessage: 'Your name must be composed only with letter and numbers',
+            },
+          }}
+        />
+      </Label>
+    </AvGroup>
   ));
   return (
     <div className='login-form-wrapper'>
-      <form className='login-form' onSubmit={onSubmit}>
+      <AvForm className='login-form' onSubmit={onSubmit}>
         <h3>{isRegisterMode ? 'Register:' : 'Login:'}</h3>
         {inputsField}
         <SubmitButton isFormValid={isFormValid} onClick={onSubmit}>
           {isRegisterMode ? 'Register' : 'Login'}
         </SubmitButton>
-      </form>
+      </AvForm>
       <SuccessButton onClick={onSwitchMode}>{isRegisterMode ? 'Switch to login' : 'Switch to register'}</SuccessButton>
     </div>
   );
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,8 +2,10 @@ const validation = (data, inputs) => {
   const keys = Object.keys(data);
   const isValidArray = keys.map((key) => {
     const searchedInput = inputs.find(({ id }) => id === key);
-    console.log(key, searchedInput.validationPattern.test(data[key]));
-    return searchedInput.validationPattern.test(data[key]);
+    const regExp = new RegExp(searchedInput.validationPattern);
+    console.log(regExp);
+    console.log(key, regExp.test(data[key]));
+    return regExp.test(data[key]);
   });
   return isValidArray.reduce((prev, current) => prev && current);
 };
