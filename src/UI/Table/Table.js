5b56af71ff395d8739feb02e251bf05f3abc893f
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-04-30 12:44:41 +0300

    refactor: delete return in table

--- src/UI/Table/Table.js
+++ src/UI/Table/Table.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Table = ({ children }) => {
-  return <table className='members-table table'>{children}</table>;
-};
+const Table = ({ children }) => <table className='members-table table'>{children}</table>;
 
 Table.propTypes = {
   children: PropTypes.element.isRequired,
