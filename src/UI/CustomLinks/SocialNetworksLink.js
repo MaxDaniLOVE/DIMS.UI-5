effc7f1079fc57681fc991e257d36972a5e0a990
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-06-18 13:14:07 +0300

    refactor: add default props to network links

--- src/UI/CustomLinks/SocialNetworksLink.js
+++ src/UI/CustomLinks/SocialNetworksLink.js
@@ -2,17 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from './Link';
 
-const SocialNetworksLink = ({ children, href }) => {
+const SocialNetworksLink = ({ children, href, target, rel }) => {
   return (
-    <Link href={href} target='_blank' rel='noopener noreferrer'>
+    <Link href={href} target={target} rel={rel}>
       {children}
     </Link>
   );
 };
 
+SocialNetworksLink.defaultProps = {
+  target: '_blank',
+  rel: 'noopener noreferrer',
+};
+
 SocialNetworksLink.propTypes = {
   children: PropTypes.element.isRequired,
   href: PropTypes.string.isRequired,
+  target: PropTypes.string,
+  rel: PropTypes.string,
 };
 
 export default SocialNetworksLink;
