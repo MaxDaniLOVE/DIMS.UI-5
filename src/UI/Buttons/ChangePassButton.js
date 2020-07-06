ad7bd3c489c988b832247dab9949175fb968318d
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-07-06 17:15:06 +0300

    refactor: rewrite ChangePassButton

--- src/UI/Buttons/ChangePassButton.js
+++ src/UI/Buttons/ChangePassButton.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Tooltip } from 'reactstrap';
 import { EditUserIcon } from '../../assets/icons';
+import Button from './Button';
+import { useTooltipToggling } from '../../hooks';
 
 const ChangePassButton = ({ onClick, children, isPasswordAuth }) => {
+  const [isOpenTooltip, toggleTooltip] = useTooltipToggling();
+
   const icon = !isPasswordAuth && <EditUserIcon />;
+
+  const onClickHandler = !isPasswordAuth ? onClick : null;
+
+  const tooltip = isPasswordAuth && (
+    <Tooltip placement='bottom' isOpen={isOpenTooltip} target='change-pass-btn' toggle={toggleTooltip}>
+      To change password login with password and email!
+    </Tooltip>
+  );
+
   return (
-    <button onClick={onClick} className='change-pass-btn' type='button' disabled={isPasswordAuth}>
-      {children && (
-        <>
-          {children}
-          {icon}
-        </>
-      )}
-    </button>
+    <>
+      <Button onClick={onClickHandler} id='change-pass-btn'>
+        {children && (
+          <>
+            {children}
+            {icon}
+          </>
+        )}
+      </Button>
+      {tooltip}
+    </>
   );
 };
 
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,6 @@
 import useAlert from './useAlert';
 import useModalToggling from './useModalToggling';
 import useLayoutEffect from './useLayoutEffect';
+import useTooltipToggling from './useTooltipToggling';
 
-export { useAlert, useModalToggling, useLayoutEffect };
+export { useAlert, useModalToggling, useLayoutEffect, useTooltipToggling };
