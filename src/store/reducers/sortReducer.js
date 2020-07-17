e08023a1be40d627d96e908eeaae10837d540eff
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-07-17 15:31:33 +0300

    fix: fix resetting sort bug

--- src/store/reducers/sortReducer.js
+++ src/store/reducers/sortReducer.js
@@ -1,14 +1,22 @@
 /* eslint-disable no-case-declarations */
 import { SORT_DATA, RESET_SORT, FILTER_DATA, SET_FILTER } from '../actions/actionTypes';
 
-const initialState = {
+const sortInititalState = {
   sortedData: [],
   sortInfo: {},
   isSorted: false,
+};
+
+const filterInitialState = {
   isFiltered: false,
   filteredData: [],
   filterInfo: {},
   settedFilters: {},
+};
+
+const initialState = {
+  ...sortInititalState,
+  ...filterInitialState,
 };
 
 const sortReducer = (state = initialState, { type, payload }) => {
@@ -17,7 +25,7 @@ const sortReducer = (state = initialState, { type, payload }) => {
       const { sortedData, sortInfo } = payload;
       return { ...state, isSorted: true, sortedData, sortInfo };
     case RESET_SORT:
-      return { ...initialState };
+      return { ...state, ...sortInititalState };
     case FILTER_DATA:
       return { ...state, isFiltered: true, settedFilters: payload.settedFilters, filteredData: payload.filteredData };
     case SET_FILTER:
