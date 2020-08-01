5972c0aa6ad863f5d83f748e4a26db0d6b3981a9
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-08-01 16:59:21 +0300

    feat: add styles for AgeInputs

--- src/components/AgeInputs/AgeInputs.js
+++ src/components/AgeInputs/AgeInputs.js
@@ -2,29 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Input, Label } from 'reactstrap';
 
+import './ageInputs.scss';
+
 const AgeInputs = ({ options, onChange, minAge, maxAge }) => {
   return (
-    <div className='age=inputs'>
-      {options.map((option) => {
-        const value = option === 'minAge' ? minAge : maxAge;
-        const min = option === 'minAge' ? 18 : minAge;
-        const max = option === 'minAge' ? maxAge : 100;
-        const placeholder = option === 'minAge' ? 'from' : 'to';
-        return (
-          <Label key={option} htmlFor={option}>
-            {option}
-            <Input
-              placeholder={placeholder}
-              id={option}
-              type='number'
-              onChange={onChange}
-              value={value}
-              min={min}
-              max={max}
-            />
-          </Label>
-        );
-      })}
+    <div className='age-inputs'>
+      <Label key='age' htmlFor='age'>
+        Age:
+        <div className='age-inputs__wrapper'>
+          {options.map((option) => {
+            const value = option === 'minAge' ? minAge : maxAge;
+            const min = option === 'minAge' ? 18 : minAge;
+            const max = option === 'minAge' ? maxAge : 100;
+            const placeholder = option === 'minAge' ? 'from:' : 'to:';
+            return (
+              <Input
+                key={option}
+                placeholder={placeholder}
+                id={option}
+                type='number'
+                onChange={onChange}
+                value={value}
+                min={min}
+                max={max}
+              />
+            );
+          })}
+        </div>
+      </Label>
     </div>
   );
 };
